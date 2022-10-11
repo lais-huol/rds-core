c7605181d0f1f4e51276d05b3333a122cdbb5159
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2022-10-11 17:43:04 -0300

    feat: del: 100% de cobertura de código no search_engine

--- bds_framework/searchengine/__init__.py
+++ bds_framework/searchengine/__init__.py
@@ -1,68 +1,101 @@
-import importlib
-from xmlrpc.client import Boolean
-# from config import settings
-from ..helpers import get_variable_by_pathname, instantiate_class
+import datetime
+from typing import Dict, Union, Any, List
+import opensearchpy
+import elasticsearch
+from dynaconf.utils.boxing import DynaBox
+from bds_framework.config import settings
+from bds_framework.helpers import instantiate_class
 
 
 __search_engine_cache = {}
 
 
-def get_search_engine_config(alias:str='default'):
-    assert alias in settings.SEARCH_ENGINES, f"Não existe um search engine com o nome apelido '{alias}' nas settings"
+def get_search_engine_config(alias: str = 'default') -> DynaBox:
+    assert settings.SEARCH_ENGINES and alias in settings.SEARCH_ENGINES, \
+        f"Não existe um search engine com o apelido '{alias}' nas settings"
     return settings.SEARCH_ENGINES[alias]
 
 
-def dsl_connection(alias:str='default'):
+def search_engine(alias: str = 'default') -> Union[opensearchpy.OpenSearch, elasticsearch.Elasticsearch]:
     se_config = get_search_engine_config(alias)
-
-    dsl_connections = get_variable_by_pathname(se_config['dsl_engine'])
-    if alias not in dsl_connections._conns:
-        dsl_connections.add_connection(alias, search_engine(alias))
-    # dsl_connections.create_connection(
-    #     alias=alias,
-    #     http_auth=(se_config['user'], se_config['password']), 
-    #     **{k:v for k,v in se_config.items() if k not in ('user', 'password', 'engine', 'dsl_engine')},
-    # )
-    return dsl_connections.get_connection(alias)
-    
-    
-def search_engine(alias:str='default'):
-    assert alias in settings.SEARCH_ENGINES, f"Não existe um search engine com o nome apelido '{alias}' nas settings"
-    se_config = settings.SEARCH_ENGINES[alias]
 
     if alias not in __search_engine_cache:
         engine = se_config['engine']
-        params = {k:v for k,v in se_config.items() if k not in ('username', 'password', 'engine', 'dsl_engine', 'hosts')}
-        client = instantiate_class(engine, se_config['hosts'].split(','), http_auth=(se_config['username'], se_config['password']), **params)
+        params = {k: v for k, v in se_config.items() if
+                  k not in ('username', 'password', 'engine', 'dsl_engine', 'hosts')}
+        client = instantiate_class(engine, se_config['hosts'].split(','),
+                                   http_auth=(se_config['username'], se_config['password']), **params)
         __search_engine_cache[alias] = client
     return __search_engine_cache[alias]
 
 
-def create_index_if_not_exists(index_name: str) -> bool:
+# def dsl_connection(alias: str = 'default') -> Union[opensearchpy.OpenSearch, elasticsearch.Elasticsearch]:
+#     se_config = get_search_engine_config(alias)
+#
+#     dsl_connections = get_variable_by_pathname(se_config['dsl_engine'])
+#     if alias not in dsl_connections.connections:
+#         dsl_connections.add_connection(alias, search_engine(alias))
+#     # dsl_connections.create_connection(
+#     #     alias=alias,
+#     #     http_auth=(se_config['user'], se_config['password']),
+#     #     **{k:v for k,v in se_config.items() if k not in ('user', 'password', 'engine', 'dsl_engine')},
+#     # )
+#     return dsl_connections.get_connection(alias)
+
+
+def create_index_if_not_exists(index_name: str, alias: str = 'default', **kwargs) -> bool:
     """ Cria um índice caso não exista. Retorna True se criou e False se não criou.
 
-    Args:
-        index_name (str): nome do índice a ser criado
+        Args:
+            index_name (str): nome do índice a ser criado
+            alias (str): alias para o search engine
 
-    Raises:
-        exception: Erro conforme retornado pelo Search Engine.
+        Raises:
+            exception: Erro conforme retornado pelo Search Engine.
 
-    Returns:
-        bool: True se criou. False se não criou.
+        Returns:
+            bool: True se criou. False se não criou.
     """
     try:
-        search_engine().indices.create(index_name, body={})
+        search_engine(alias).indices.create(index_name, body={}, **kwargs)
         return True
     except Exception as e:
-        if hasattr(e, 'error') and e.error == 'resource_already_exists_exception':
+        if getattr(e, 'error') == 'resource_already_exists_exception':
             return False
         raise e
 
 
-def query(index_name: str, query_string: str, fields: list, alias="default") -> tuple:
+def delete_index_if_exists(index_name: str, alias: str = 'default', **kwargs) -> bool:
+    """ Apaga um índice caso exista. Retorna True se apagou e False se não apagou.
+
+        Args:
+            index_name (str): nome do índice a ser criado
+            alias (str): alias para o search engine
+
+        Raises:
+            exception: Erro conforme retornado pelo Search Engine.
+
+        Returns:
+            bool: True se criou. False se não criou.
+    """
+    try:
+        search_engine(alias).indices.delete(index_name, **kwargs)
+        return True
+    except Exception as e:
+        if getattr(e, 'error') == 'index_not_found_exception':
+            return False
+        raise e
+
+
+def query(index_name: str,
+          query_string: Union[str, int, float, datetime.date, datetime.datetime],
+          fields: Union[List, None] = None,
+          alias: str = "default") -> Any:
+    if fields is None:
+        fields = []
     response = search_engine(alias).search(
-        index = index_name,
-        body = {
+        index=index_name,
+        body={
             'size': 5,
             'query': {
                 'multi_match': {
@@ -75,25 +108,27 @@ def query(index_name: str, query_string: str, fields: list, alias="default") -> 
     return response['hits']['hits'], response['hits']['total']['value']
 
 
-def search(index_name: str, body: dict, alias="default") -> tuple:
+def search(index_name: str, body: dict, alias: str = "default") -> Any:
     response = search_engine(alias).search(
-        index = index_name,
-        body = body
+        index=index_name,
+        body=body
     )
     return response['hits']['hits'], response['hits']['total']['value']
 
 
-def index(index_name: str, body: str, id=None, params=None, headers=None, alias:str="default") -> tuple:
+def index(index_name: str,
+          body: Dict,
+          _id: Any = None,
+          alias: str = "default",
+          **kwargs) -> Dict:
     response = search_engine(alias).index(
-        index = index_name,
-        body = body,
-        id=id, 
-        params=params, 
-        headers=headers
+        index=index_name,
+        body=body,
+        id=_id,
+        **kwargs
     )
-    return response['hits']['hits'], response['hits']['total']['value']
+    return response
 
 
-def search_engine_healthy(alias:str='default'):
-    p = search_engine(alias).ping()
-    return p != False
+def search_engine_healthy(alias: str = 'default') -> bool:
+    return search_engine(alias).ping()
---
+++ tests/test_searchengine.py
@@ -0,0 +1,104 @@
+import time
+import unittest
+
+import dynaconf
+import opensearchpy
+import pytest
+
+from bds_framework.searchengine import (
+    create_index_if_not_exists, delete_index_if_exists,
+    get_search_engine_config, search_engine, search_engine_healthy,
+    index, query, search,
+    # dsl_connection,
+)
+
+INVALID_ALIAS = "Não existe um search engine com o apelido 'fail' nas settings"
+
+
+class TestHelpersInit(unittest.TestCase):
+
+    def __init__(self, method_nname: str = 'runTest'):
+        self.test_index = 'test_index'
+        super(TestHelpersInit, self).__init__(methodName=method_nname)
+
+    def setUp(self):
+        create_index_if_not_exists(self.test_index)
+
+    def tearDown(self):
+        delete_index_if_exists(self.test_index)
+
+    def test_double_create_index(self):
+        self.assertFalse(create_index_if_not_exists(self.test_index))
+
+    def test_invalid_index_creation(self):
+        with pytest.raises(Exception):
+            create_index_if_not_exists(self.test_index, fail=True)
+
+    def test_delete_index_not_exists(self):
+        self.assertFalse(delete_index_if_exists('nem_criei_ainda'))
+
+    def test_invalid_index_deletion(self):
+        with pytest.raises(Exception):
+            delete_index_if_exists('nem_criei_ainda', fail=True)
+
+    def test_get_search_engine_config_valid(self):
+        self.assertIsInstance(get_search_engine_config(), dynaconf.utils.boxing.DynaBox)
+        self.assertIsInstance(get_search_engine_config('default'), dynaconf.utils.boxing.DynaBox)
+
+    def test_get_search_engine_config__invalid(self):
+        with self.assertRaisesRegex(AssertionError, INVALID_ALIAS):
+            self.assertIsNone(get_search_engine_config('fail'))
+
+    # @openmock
+    # def test_dsl_connection(self):
+    #     self.assertEqual(dsl_connection(), unittest.TestCase)
+    #     self.assertEqual(dsl_connection('default'), unittest.TestCase)
+    #     # self.assertEqual(dsl_connection('does-not-exist'), unittest.TestCase)
+
+    def test_search_engine(self):
+        self.assertIsInstance(search_engine(), opensearchpy.OpenSearch)
+        self.assertIsInstance(search_engine('default'), opensearchpy.OpenSearch)
+
+    def test_search_engine__invalid(self):
+        with self.assertRaisesRegex(AssertionError, INVALID_ALIAS):
+            search_engine('fail')
+
+    def test_search_engine_healthy(self):
+        self.assertTrue(search_engine_healthy())
+        self.assertTrue(search_engine_healthy('default'))
+
+    def test_search_engine_healthy__invalid(self):
+        with self.assertRaisesRegex(AssertionError, INVALID_ALIAS):
+            search_engine_healthy('fail')
+
+    # @openmock
+    def test_index_document(self):
+        self.assertIsInstance(index(self.test_index, {'username': 'admin', 'firstname': 'Admin'}), dict)
+        self.assertIsInstance(index(self.test_index, {'username': 'admin', 'firstname': 'Admin'}), dict)
+        self.assertIsInstance(index(self.test_index, {'username': 'admin', 'firstname': 'Admin'}, '1'), dict)
+        self.assertIsInstance(index(self.test_index, {'username': 'admin', 'firstname': 'Admin'}, '2', 'default'), dict)
+
+    def test_query(self):
+        index(self.test_index, {'username': 'admin', 'firstname': 'Admin'})
+        time.sleep(2)  # como é próximo ao real, tem que esperar terminar de indexar
+        result, count = query(self.test_index, 'admin')
+        self.assertEqual(count, 1)
+
+        index(self.test_index, {'username': 'admin', 'firstname': 'Admin2'}, alias='default')
+        time.sleep(2)
+        result, count = query(self.test_index, 'admin')
+        self.assertEqual(count, 2)
+
+    def test_search(self):
+        index(self.test_index, {'username': 'admin', 'firstname': 'Admin'})
+        time.sleep(2)  # como é próximo ao real, tem que esperar terminar de indexar
+        body = {
+            'size': 5,
+            'query': {
+                'multi_match': {
+                    'query': 'admin',
+                }
+            }
+        }
+        result, count = search(self.test_index, body)
+        self.assertEqual(count, 1)
