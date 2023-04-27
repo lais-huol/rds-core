8693d0bc3b15c6e403a87ffeccd13ff74ad1ad4d
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-04-26 21:53:45 -0300

    build: [add] typing files

---
+++ rds_core/cache/__init__.pyi
@@ -0,0 +1,6 @@
+from _typeshed import Incomplete
+from rds_core.config import settings as settings
+from rds_core.helpers import instantiate_class as instantiate_class
+
+caches: Incomplete
+default_cache: Incomplete
--- rds_core/config/__init__.py
+++ rds_core/config/__init__.py
@@ -6,5 +6,5 @@ import dynaconf
 
 settings = dynaconf.Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=['settings.yaml', '.secrets.toml'],
+    settings_files=["settings.yaml", "settings.toml", ".secrets.toml"],
 )
