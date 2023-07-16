de50a1b4f3ea10c2748e4094593ae11059333e3e
Kelson daCosta Medeiros <kelsoncm@gmail.com>
2023-07-16 16:07:14 -0300

    feat: [add] IntegerField & FloatField

--- rds/core/transformers/fields/__init__.py
+++ rds/core/transformers/fields/__init__.py
@@ -11,21 +11,33 @@ logger.setLevel(logging.INFO)
 
 
 class Field:
-    def __init__(self, source_field: Union[str, object, None]):
+    def __init__(self, source_field: Union[str, object, None], none_as: Any = None):
         self.source_field = source_field
+        self.none_as = none_as
 
-    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str):
+    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str) -> None:
         if self.source_field is None:
             dest_dict[dest_fieldname] = None
         else:
             dest_dict[dest_fieldname] = self.cast_source_value(get_dict_by_pathname(source_dict, self.source_field))
 
-    def cast_source_value(self, source_value: Union[str, int, bool, float]):
-        return source_value
+    def cast_source_value(self, source_value: Union[str, int, bool, float]) -> Any:
+        return source_value if source_value is not None else self.none_as
 
 
 class CharField(Field):
-    pass
+    def cast_source_value(self, source_value: Union[str, int, bool, float]) -> str:
+        return str(source_value) if source_value is not None else self.none_as
+
+
+class IntegerField(Field):
+    def cast_source_value(self, source_value: Union[None, str, int, float]) -> int:
+        return int(source_value)
+
+
+class FloatField(Field):
+    def cast_source_value(self, source_value: Union[None, str, int, float]) -> float:
+        return float(source_value)
 
 
 class DateField(Field):
@@ -59,18 +71,18 @@ class SimpleConcatField(Field):
             )
 
 
-class SubModelField(Field):
-    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str):
-        dest_dict[dest_fieldname] = self.source_field.transform_to_dict(source_dict)
-
-
 class FixedField(Field):
     def __init__(self, fixed_value: str):
         super().__init__(None)
         self.fixed_value = fixed_value
 
-    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str):
+    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str) -> None:
         dest_dict[dest_fieldname] = self.cast_source_value(self.fixed_value)
 
-    def cast_source_value(self, source_value: Union[None, str, int, bool, float]) -> datetime.date:
-        return self.fixed_value
+    def cast_source_value(self, source_value: Any) -> Any:
+        return source_value
+
+
+class SubModelField(Field):
+    def transform_to_dict(self, source_dict: Dict[str, Any], dest_dict: Dict[str, Any], dest_fieldname: str):
+        dest_dict[dest_fieldname] = self.source_field.transform_to_dict(source_dict)
