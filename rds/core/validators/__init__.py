90a52a1f4d36353e3548a75f24d0355247f993ef
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2024-03-13 08:36:58 -0300

    feat: [add] valida cnpj [add] tipo de retorno INVALIDO

--- rds/core/validators/__init__.py
+++ rds/core/validators/__init__.py
@@ -1,6 +1,6 @@
 from typing import Union
 import re
-from rds.core.types import CPFCNS
+from rds.core.types import CPFCNS, CPFCNSCNPJ
 
 RE_CNS = re.compile("^\d{15}$")
 RE_CPF = re.compile("^\d{11}$")
@@ -26,4 +26,17 @@ def validate_cns_cpf(value: str) -> Union[CPFCNS, None]:
     if validate_cns(value):
         return CPFCNS.CNS
 
-    return None
+    return CPFCNS.INVALIDO
+
+
+def validate_cns_cpf_cnpj(value: str) -> Union[CPFCNS, None]:
+    if validate_cpf(value):
+        return CPFCNSCNPJ.CPF
+
+    if validate_cns(value):
+        return CPFCNSCNPJ.CNS
+
+    if validate_cnpj(value):
+        return CPFCNSCNPJ.CNS
+
+    return CPFCNSCNPJ.INVALIDO
